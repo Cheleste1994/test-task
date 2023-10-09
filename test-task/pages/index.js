70acf886c1176b6c3856d983d631b0fcbac35a86
Cheleste1994 <wolverines@tut.by>
2023-10-09 21:02:11 +0300

    feat: add page login

--- test-task/pages/index.js
+++ test-task/pages/index.js
@@ -1,21 +1,29 @@
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import { Inter } from 'next/font/google';
+import styles from '../styles/main.module.scss';
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClickBtn = (route) => {
+    router.push(route);
+  }
+
   return (
     <>
       <Header />
-      <main className='flex grow flex-col px-[30px]'>
+      <main className={`${styles.main} px-[30px]`}>
         <div className='pl-[10px] text-2xl font-bold'>Выберите действие</div>
-        <div className='mt-[80px] flex h-[65px] flex-col justify-center bg-gradient-to-br from-[#FF8412] to-[#FFC543] text-center rounded-[50px] text-white'>
+        <button className='mt-[80px] from-[#FF8412] to-[#FFC543]' onClick={() => handleClickBtn('login')}>
           Login
-        </div>
-        <div className='mt-[20px] flex h-[65px] flex-col justify-center bg-gradient-to-br from-[#686DE0] to-[#4834D4] text-center rounded-[50px] text-white'>
+        </button>
+        <button className='mt-[20px] from-[#686DE0] to-[#4834D4]' onClick={() => handleClickBtn('registration')}>
           Registration
-        </div>
+        </button>
       </main>
       <Footer />
     </>
